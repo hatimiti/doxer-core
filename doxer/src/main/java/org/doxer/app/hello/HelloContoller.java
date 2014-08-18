6cef8f54059c1e4eab68159eeb2f758db6dd07c3
マサト <@192.168.11.6>
2014-08-18 23:28:32 +0900

    sample:inputメソッド追加

--- doxer/src/main/java/org/doxer/app/hello/HelloContoller.java
+++ doxer/src/main/java/org/doxer/app/hello/HelloContoller.java
@@ -14,7 +14,13 @@ public class HelloContoller {
 
 	@RequestMapping("/hello")
 	public String hello (@ModelAttribute("form") HelloModel model) {
-		System.out.println("Hello");
+		System.out.println("HelloY");
+		return "/hello/hello.html";
+	}
+
+	@RequestMapping("input")
+	public String input(@ModelAttribute("form") HelloModel model) {
+		System.out.println(model.getVal());
 		return "/hello/hello.html";
 	}
 }
