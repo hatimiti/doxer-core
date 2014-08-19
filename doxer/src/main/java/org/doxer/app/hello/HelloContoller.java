c5488812b8948f68eb5cf7e8c7dc7cc2679bc05f
マサト <@m-kakimi-vaio>
2014-08-19 20:19:58 +0900

    log出力ライブラリ(slf4j,logback)の依存追加

--- doxer/src/main/java/org/doxer/app/hello/HelloContoller.java
+++ doxer/src/main/java/org/doxer/app/hello/HelloContoller.java
@@ -2,6 +2,8 @@ package org.doxer.app.hello;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloContoller {
 
+	protected static Logger log = LoggerFactory.getLogger(HelloContoller.class);
+
 	@Resource
 	public HelloService helloService;
 
 	@RequestMapping("/hello")
 	public String hello (@ModelAttribute("form") HelloModel model) {
 		System.out.println("HelloY");
+		log.info("ログ出力テスト");
 		return "/hello/hello.html";
 	}
 
