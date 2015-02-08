99b579d6355ef54e6e5317bbfe429ab197a4880a
hatimiti <hatti33@gmail.com>
2015-02-08 15:49:45 +0900

    トランザクション制御の仕組みを導入 ・TransactionInterceptor ・@Transactional

--- doxer/src/main/java/org/doxer/app/sample/hello/HelloService.java
+++ doxer/src/main/java/org/doxer/app/sample/hello/HelloService.java
@@ -1,17 +1,32 @@
 package org.doxer.app.sample.hello;
 
+import static org.springframework.context.annotation.ScopedProxyMode.*;
+
 import javax.annotation.Resource;
 
 import org.doxer.app.db.dbflute.exbhv.TcmSampleBhv;
+import org.doxer.app.db.dbflute.exentity.TcmSample;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(proxyMode = TARGET_CLASS)
 public class HelloService {
 
 	@Resource
 	public TcmSampleBhv tcmSampleBhv;
-
+	
 	public void search(HelloModel model) {
 		model.setResults(tcmSampleBhv.findBySampleName(model.getVal()));
+
+		if ("register".equals(model.getVal().getVal())) {
+			register(model);
+		}
+	}
+
+	public void register(HelloModel model) {
+		TcmSample entity = new TcmSample();
+		entity.setSampleName("hatimiti");
+		tcmSampleBhv.insert(entity);
 	}
 }
---
+++ doxer/src/main/java/org/doxer/xbase/aop/interceptor/DoxTransactionInterceptor.java
@@ -0,0 +1,24 @@
+package org.doxer.xbase.aop.interceptor;
+
+import java.lang.reflect.Method;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionInterceptor;
+
+/**
+ * @Transactional アノテーションが付加されたメソッドの場合はそちらの設定を優先する。
+ * それ以外の動作についてはデフォルトのTransactionInterceptorの動作に従う。
+ * @author m-kakimi
+ */
+@SuppressWarnings("serial")
+public class DoxTransactionInterceptor extends TransactionInterceptor {
+
+	@Override
+	public Object invoke(final MethodInvocation invocation) throws Throwable {
+		Method method = invocation.getMethod();
+		Transactional transactional = method.getAnnotation(Transactional.class);
+		return transactional != null ? invocation.proceed() : super.invoke(invocation);
+	}
+}
+
