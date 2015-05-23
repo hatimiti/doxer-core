02b15af85d31a3787829209b4aa070b3296a488b
hatimiti <hatti33@gmail.com>
2015-05-23 19:35:34 +0900

    拡張ポイント追加

---
+++ doxer/src/main/java/org/doxer/xbase/servlet/mvc/method/annotation/DoxRequestMappingHandlerAdapter.java
@@ -0,0 +1,6 @@
+package org.doxer.xbase.servlet.mvc.method.annotation;
+
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+
+public class DoxRequestMappingHandlerAdapter extends RequestMappingHandlerAdapter {
+}
---
+++ doxer/src/main/java/org/doxer/xbase/servlet/mvc/method/annotation/DoxRequestMappingHandlerMapping.java
@@ -0,0 +1,7 @@
+package org.doxer.xbase.servlet.mvc.method.annotation;
+
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+public class DoxRequestMappingHandlerMapping extends RequestMappingHandlerMapping {
+
+}
