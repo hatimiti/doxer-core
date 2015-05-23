b6898aa5e7563d85e2729c3dd1e0d10ace0dd430
hatimiti <hatti33@gmail.com>
2015-05-23 23:09:06 +0900

    Loggerを基底クラスへ移動

--- doxer/src/main/java/org/doxer/app/sample/hello/HelloController.java
+++ doxer/src/main/java/org/doxer/app/sample/hello/HelloController.java
@@ -5,18 +5,13 @@ import javax.annotation.Resource;
 import org.doxer.xbase.controller.DoxController;
 import org.doxer.xbase.form.AccessUser;
 import org.doxer.xbase.util._Container;
-import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.github.hatimiti.flutist.common.util._Obj;
-
 @Controller
 @RequestMapping("/sample/hello")
 public class HelloController extends DoxController {
-
-	private static Logger log = _Obj.getLogger();
 
 	@Resource
 	public HelloService helloService;
@@ -26,8 +21,8 @@ public class HelloController extends DoxController {
 
 	@RequestMapping("/index")
 	public String index(HelloForm form) {
-		log.info("ログ出力テスト 時間={}", _Container.getAccessDate());
-		log.info("user = {}", accessUser);
+		LOG.info("ログ出力テスト 時間={}", _Container.getAccessDate());
+		LOG.info("user = {}", accessUser);
 		accessUser.setNameMei("hatimiti");
 		return view("/hello/hello", form);
 	}
@@ -35,7 +30,7 @@ public class HelloController extends DoxController {
 	@RequestMapping("/input")
 	public String input(HelloForm form) {
 		this.helloService.search(form);
-		log.info("ログ出力テスト2, {}", form.getResults());
+		LOG.info("ログ出力テスト2, {}", form.getResults());
 		return view("/hello/hello", form);
 	}
 	
--- doxer/src/main/java/org/doxer/xbase/controller/DoxController.java
+++ doxer/src/main/java/org/doxer/xbase/controller/DoxController.java
@@ -6,13 +6,17 @@ import static org.doxer.xbase.util._Container.*;
 import javax.servlet.http.HttpServletRequest;
 
 import org.doxer.xbase.form.Form;
+import org.slf4j.Logger;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.github.hatimiti.flutist.common.util._Obj;
 
 @Scope("request")
 public abstract class DoxController {
 
+	protected static Logger LOG = _Obj.getLogger();
+	
 	protected String view(String path, Form form) {
 		HttpServletRequest req = getHttpServletRequest();
 		req.setAttribute(MODEL_AND_VIEW_ATTRIBUTE_FORM_KEY, form);
