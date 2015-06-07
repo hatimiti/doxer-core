aac8905b1e27a0a9a6522080537f667ab32ce852
hatimiti <hatti33@gmail.com>
2015-06-07 11:04:31 +0900

    ファイルダウンロード処理追加

--- doxer/src/main/java/org/doxer/xbase/servlet/DoxDispatcherServlet.java
+++ doxer/src/main/java/org/doxer/xbase/servlet/DoxDispatcherServlet.java
@@ -9,15 +9,31 @@ import org.springframework.web.servlet.ModelAndView;
 public class DoxDispatcherServlet extends DispatcherServlet {
 
 	public static final String MODEL_AND_VIEW_FORM_KEY = "__DOX_MODEL_AND_VIEW_FORM_KEY__";
-	
+
 	@Override
 	protected void render(
 			ModelAndView mv,
 			HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		
+
+		if (!mv.hasView()) {
+			// getDefaultViewName() メソッドで null が返された際の対応
+			return;
+		}
+
 		mv.addObject("form", request.getAttribute(MODEL_AND_VIEW_FORM_KEY));
 		super.render(mv, request, response);
 	}
-	
+
+	@Override
+	protected String getDefaultViewName(HttpServletRequest request)
+			throws Exception {
+		/*
+		 * ファイルダウンロード処理実装時にコントローラ戻り値をnullにした際に
+		 * 当メソッドでデフォルトのView名が設定され、レンダリング処理に進んでしまうため
+		 * nullでオーバーライドすることとする．
+		 */
+		return null;
+	}
+
 }
--- doxer/src/main/java/org/doxer/xbase/util/_Container.java
+++ doxer/src/main/java/org/doxer/xbase/util/_Container.java
@@ -1,5 +1,8 @@
 package org.doxer.xbase.util;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Path;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
@@ -14,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.doxer.xbase.form.AccessUser;
+import org.slf4j.Logger;
 import org.springframework.context.MessageSource;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -24,7 +28,11 @@ import com.github.hatimiti.flutist.common.message.AppMessage;
 import com.github.hatimiti.flutist.common.message.AppMessagesContainer;
 import com.github.hatimiti.flutist.common.message.GlobalMessages;
 import com.github.hatimiti.flutist.common.message.OwnedMessages;
+import com.github.hatimiti.flutist.common.util.CharacterEncoding;
+import com.github.hatimiti.flutist.common.util.MIMEType;
 import com.github.hatimiti.flutist.common.util._Date;
+import com.github.hatimiti.flutist.common.util._Http;
+import com.github.hatimiti.flutist.common.util._Obj;
 import com.github.hatimiti.flutist.common.util._Str;
 
 /**
@@ -34,13 +42,15 @@ import com.github.hatimiti.flutist.common.util._Str;
  */
 public final class _Container {
 
+	private static final Logger LOG = _Obj.getLogger();
+
 	/* ****************************************
 	 * フィールド定義
 	 * ****************************************/
-	
+
 	public static final String TRANSITION_REDIRECT_PREFIX = "redirect:";
 	public static final String TRANSITION_FORWARD_PREFIX = "forward:";
-	
+
 	/** アクセス時間保持(Web側で使用される日時) */
 	private static final ThreadLocal<Date> ACCESS_DATE = new ThreadLocal<Date>() {
 		@Override
@@ -48,7 +58,7 @@ public final class _Container {
 			return new Date();
 		};
 	};
-	
+
 	/** アプリケーションメッセージ保持 */
 	private static final ThreadLocal<AppMessagesContainer> APPMESSAGES
 			= new ThreadLocal<AppMessagesContainer>() {
@@ -57,16 +67,16 @@ public final class _Container {
 			return new AppMessagesContainer();
 		};
 	};
-	
+
 	/*
 	 * private コンストラクタ
 	 */
 	private _Container() { }
-	
+
 	/* ****************************************
 	 * アクセス日付
 	 * ****************************************/
-	
+
 	/**
 	 * アクセス時間を取得
 	 */
@@ -133,18 +143,18 @@ public final class _Container {
 	public static void resetAccessDate() {
 		ACCESS_DATE.remove();
 	}
-	
+
 	/* ****************************************
 	 * アプリケーションメッセージ
 	 * ****************************************/
-	
+
 	/**
 	 * アプリケーションメッセージコンテナを取得<br>
 	 */
 	public static AppMessagesContainer getAppMessagesContainer() {
 		return APPMESSAGES.get();
 	}
-	
+
 	/**
 	 * アプリケーションメッセージコンテナをクリア<br>
 	 * filter が呼び出すため、実装者が呼び出す必要はない．
@@ -152,25 +162,25 @@ public final class _Container {
 	public static void resetAppMessagesContainer() {
 		APPMESSAGES.remove();
 	}
-	
+
 	public static List<String> getGlobalMessages() {
 		return buildMessages(getAppMessagesContainer().getGlobalMessages());
 	}
-	
+
 	public static List<String> getParsedOwnedMessages(String owner) {
 		return buildMessages(getAppMessagesContainer().getOwnedMessages(owner));
 	}
-	
+
 	public static String buildMessage(String key, Object... params) {
 		Objects.requireNonNull(key);
 		MessageSource source = getComponent(MessageSource.class).get();
 		return source.getMessage(key, params, getAccessUser().getLocale());
 	}
-	
+
 	public static void addMessage(AppMessage message) {
 		APPMESSAGES.get().add(new GlobalMessages(message));
 	}
-	
+
 	public static void addMessage(String owner, AppMessage message) {
 		APPMESSAGES.get().add(new OwnedMessages(owner, message));
 	}
@@ -189,7 +199,7 @@ public final class _Container {
 	/* ****************************************
 	 * アプリケーションWeb設定
 	 * ****************************************/
-	
+
 	/**
 	 * アプリケーションルートパスを取得する．
 	 * @return アプリケーションルートフォルダパス
@@ -201,11 +211,11 @@ public final class _Container {
 	public static ServletContext getServletContext() {
 		return getHttpServletRequest().getServletContext();
 	}
-	
+
 	public static WebApplicationContext getWebApplicationContext() {
 		return RequestContextUtils.getWebApplicationContext(getHttpServletRequest());
 	}
-	
+
 	public static String getContextPath() {
 		return getHttpServletRequest().getContextPath();
 	}
@@ -238,11 +248,29 @@ public final class _Container {
 //		return af != null;
 //		return af != null || vf != null;
 //	}
-	
+
+	/* ****************************************
+	 * ダウンロード
+	 * ****************************************/
+
+	public static void downloadFile(Path filePath) {
+		try {
+			OutputStream writer = _Http.getOutputStreamForDownload(
+					getHttpServletResponse(), CharacterEncoding.UTF8,
+					MIMEType.APPL_OCTET_STREAM, filePath.getFileName().toString());
+
+			_Http.write(writer, filePath.toFile());
+
+		} catch (IOException e) {
+			LOG.warn("Download is missed!! message = {}, stacktrace = {}",
+					e.getMessage(), e.getStackTrace());
+		}
+	}
+
 	/* ****************************************
 	 * アクセスユーザー
 	 * ****************************************/
-	
+
 	public static AccessUser getAccessUser() {
 		return getComponent(AccessUser.class).get();
 	}
@@ -250,15 +278,15 @@ public final class _Container {
 	/* ****************************************
 	 * DIコンテナ設定
 	 * ****************************************/
-	
+
 	public static String getRedirectPath(String path) {
 		return TRANSITION_REDIRECT_PREFIX + path;
 	}
-	
+
 	public static String getForwardPath(String path) {
 		return TRANSITION_FORWARD_PREFIX + path;
 	}
-	
+
 	/**
 	 * seasar 管理下のオブジェクトを取得する．
 	 * @return 指定したオブジェクト
