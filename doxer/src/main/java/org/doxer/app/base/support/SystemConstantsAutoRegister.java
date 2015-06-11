9be25247999cb0832ee33e622e904e6269e46618
hatimiti <hatti33@gmail.com>
2015-06-11 23:27:46 +0900

    ServletContextへの定数登録処理追加

--- doxer/src/main/java/org/doxer/app/base/support/SystemConstantsAutoRegister.java
+++ doxer/src/main/java/org/doxer/app/base/support/SystemConstantsAutoRegister.java
@@ -1,25 +1,32 @@
 package org.doxer.app.base.support;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.ServletContext;
 
 import org.doxer.app.db.dbflute.allcommon.CDef;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
 import com.github.hatimiti.flutist.common.support.ConstantsAutoRegister;
 
-/**
- * @author m-kakimi
- */
-public class SystemConstantsAutoRegister extends ConstantsAutoRegister {
 
-	public SystemConstantsAutoRegister(ServletContext application) {
-		super(application);
-	}
+/**
+ * @author hatimiti
+ */
+@Component
+public class SystemConstantsAutoRegister extends ConstantsAutoRegister
+		implements ApplicationListener<ContextRefreshedEvent> {
+
+	@Autowired
+	private ServletContext application;
 
 	@Override
-	protected List<TargetClassInfo> getClassPatternList() {
+	protected List<TargetClassInfo> getAdditionalClassPatterns() {
 		List<TargetClassInfo> tciList = new ArrayList<TargetClassInfo>();
 		/*
 		 * 第1引数は探索するパッケージに属する class
@@ -30,25 +37,21 @@ public class SystemConstantsAutoRegister extends ConstantsAutoRegister {
 //		tciList.add(new TargetClassInfo(SystemConstants.class, "SystemConstants"));
 //		tciList.add(new TargetClassInfo(Constants.class, "Constants"));
 		tciList.add(new TargetClassInfo(CDef.class, "CDef"));
-//		tciList.add(new TargetClassInfo(CDef.class, ".*"));
+		tciList.add(new TargetClassInfo(CDef.class, ".*"));
 		return tciList;
 	}
 
 	@Override
-	protected void processBefore(Class<?> clazz) {
-
-		if (clazz.isAssignableFrom(CDef.class)) {
-			for (Class<?> innerClazz : clazz.getClasses()) {
-				registTargetFields(innerClazz);
-			}
-		}
+	protected List<Class<?>> getAdditionalTargetTypes() {
+		List<Class<?>> targetTypes = new ArrayList<>();
+		Arrays.stream(CDef.class.getClasses()).forEach(targetTypes::add);
+		return targetTypes;
 	}
 
 	@Override
-	protected void addTargetTypes(List<Class<?>> targetTypes) {
-		for (Class<?> innerClazz : CDef.class.getClasses()) {
-			targetTypes.add(innerClazz);
-		}
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		Arrays.stream(CDef.class.getClasses())
+			.forEach(c -> register(application, c.getName()));
 	}
 
 }
