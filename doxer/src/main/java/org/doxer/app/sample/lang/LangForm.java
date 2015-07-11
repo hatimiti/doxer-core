7d63f3e7b9d74e75b349bbe352aa2367643898e9
hatimiti <hatti33@gmail.com>
2015-07-12 01:03:38 +0900

    言語設定Form修正

--- doxer/src/main/java/org/doxer/app/sample/lang/LangForm.java
+++ doxer/src/main/java/org/doxer/app/sample/lang/LangForm.java
@@ -8,12 +8,14 @@ import org.doxer.xbase.util._Container;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
+import com.github.hatimiti.flutist.common.domain.supports.Condition;
+
 @Data
 @EqualsAndHashCode(callSuper = true)
 @Component
 public class LangForm extends DoxForm {
 
-	String lang;
+	@Condition String lang;
 
 	public String getMessage() {
 		return _Container.getComponent(MessageSource.class).get().getMessage("samplemes", null, _Container.getAccessUser().getLocale());
