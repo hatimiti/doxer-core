7877ca866f5e07bbd20455787f490c3329d869f8
hatimiti <hatti33@gmail.com>
2015-08-04 23:57:45 +0900

    DoxlsによるExcelダウンロードサンプル追加

--- doxer/src/main/java/org/doxer/app/sample/hello/HelloController.java
+++ doxer/src/main/java/org/doxer/app/sample/hello/HelloController.java
@@ -1,6 +1,9 @@
 package org.doxer.app.sample.hello;
 
 import static com.github.hatimiti.flutist.common.message.AppMessageLevel.*;
+import static com.github.hatimiti.flutist.common.util.CharacterEncoding.*;
+import static com.github.hatimiti.flutist.common.util.MIMEType.*;
+import static com.github.hatimiti.flutist.common.util._Http.*;
 import static org.doxer.xbase.aop.interceptor.supports.TokenType.*;
 import static org.doxer.xbase.controller.DoxController.DoxModelAndView.*;
 import static org.doxer.xbase.report.birt.DoxBirt.BIRT_OUTPUT_FORMAT.*;
@@ -10,6 +13,10 @@ import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import javax.annotation.Resource;
 
@@ -19,11 +26,13 @@ import org.doxer.xbase.aop.interceptor.supports.Token;
 import org.doxer.xbase.controller.DoxController;
 import org.doxer.xbase.form.AccessUser;
 import org.doxer.xbase.report.birt.DoxBirt;
+import org.doxer.xbase.util._Container;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.github.hatimiti.doxsl.core.Doxls;
 import com.github.hatimiti.flutist.common.annotation.Function;
 import com.github.hatimiti.flutist.common.message.AppMessage;
 
@@ -82,8 +91,29 @@ public class HelloController extends DoxController {
 	}
 
 	@RequestMapping(value = "/download")
-	public void download(HelloForm form) {
-		downloadFile(Paths.get("//LS-XHLE38/share/var/", form.getFileName()));
+	public void downloadFile(HelloForm form) {
+		_Container.downloadFile(Paths.get("//LS-XHLE38/share/var/", form.getFileName()));
+	}
+
+	@RequestMapping(value = "/output-doxls")
+	public void outputReportDoxls(HelloForm form) throws Exception {
+		try (Doxls xls = new Doxls("templates/report/xls/nested_command_template.xls",
+				getOutputStreamForDownload(
+					getHttpServletResponse(), UTF8, APPL_OCTET_STREAM, "sample.xls"))) {
+
+			List<Employee> employees = new ArrayList<Employee>();
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MMM-dd", Locale.US);
+			employees.add( new Employee("Elsa", dateFormat.parse("1970-Jul-10"), 1500, 0.15) );
+			employees.add( new Employee("Oleg", dateFormat.parse("1973-Apr-30"), 2300, 0.25) );
+			employees.add( new Employee("Neil", dateFormat.parse("1975-Oct-05"), 2500, 0.00) );
+			employees.add( new Employee("Maria", dateFormat.parse("1978-Jan-07"), 1700, 0.15) );
+			employees.add( new Employee("John", dateFormat.parse("1969-May-30"), 2800, 0.20) );
+
+			xls.put("employees", employees);
+			xls.applyAtArea(0, "Result!A1");
+			xls.output();
+			xls.removeSheetByNameOf("Template");
+		}
 	}
 
 	@RequestMapping(value = "/output-report")
